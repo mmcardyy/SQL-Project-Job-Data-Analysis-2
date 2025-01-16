f27d6728e33ec754ced4524a8f8d0ff81650ce72
mariia melnyk <melnyk.maria099@gmail.com>
2025-01-16 12:07:36 +0100

    upload sql files

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,20 @@
+/*
+Question: What are the top-paying data analyst jobs?**
+*/
+
+SELECT 
+    job_id,
+    job_title,
+    company_dim.name as company_name,
+    job_location,
+    job_posted_date,
+    job_schedule_type,
+    salary_year_avg
+FROM 
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE 
+    job_title_short = 'Data Analyst' 
+    AND job_location = 'Anywhere'
+    AND salary_year_avg IS NOT NULL
+ORDER BY salary_year_avg DESC; 
